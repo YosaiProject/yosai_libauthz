b37e2faf956f80802023410c2dc286298ef2ae74
dowwie <darinc@gmail.com>
2017-02-04 12:08:45 -0500

    debugging types at cffi

--- include/yosai_libauthz.h
+++ include/yosai_libauthz.h
@@ -2,12 +2,12 @@
 #ifndef LIBAUTHZ_H_INCLUDED
 #define LIBAUTHZ_H_INCLUDED
 
-int is_permitted_from_string(char *required_perm,
+int is_permitted_from_string(const char *required_perm,
                              char **assigned_perms,
-                             unsigned int assigned_perms_len);
+                             int assigned_perms_len);
 
-int is_permitted_from_json(char *required_perm,
-                           const char *assigned_perms,
-                           unsigned int assigned_perms_len);
+int is_permitted_from_json(const char *required_perm,
+                           char *assigned_perms,
+                           int assigned_perms_len);
 
 #endif
--- libauthz/c_abi.py
+++ libauthz/c_abi.py
@@ -1,23 +1,29 @@
 import os
+from yosai.core import authz_abcs
+
 from ._libauthz_native import ffi as _ffi
 
-_lib = _ffi.dlopen(os.path.join(os.path.dirname(__file__), '_libauthz.so'))
 
+class RustyPermissionVerifier:  # (authz_abcs.PermissionVerifier):
+    def __init__(self):
+        self.lib = _ffi.dlopen(os.path.join(os.path.dirname(__file__),
+                               '_libauthz.so'))
 
-lib = ctypes.cdll.LoadLibrary(lib_path)
+    def is_permitted_from_json(self, required_perm, serialized_perms):
+        perms_buffer = serialized_perms.encode('utf-8')
+        result = self.lib.is_permitted_from_json(required_perm.encode('utf-8'),
+                                                 perms_buffer,
+                                                 len(perms_buffer))
+        if result == -1:
+            raise ValueError("Rust Library, libauthz, panicked!")
+        return bool(result)
 
+    def is_permitted_from_string(self, required_perm, assigned_perms):
+        encoded_perms = [x.encode('utf-8') for x in assigned_perms]
+        result = self.lib.is_permitted_from_string(required_perm.encode('utf-8'),
+                                                   encoded_perms,
+                                                   len(encoded_perms))
 
-def is_permitted_from_json(required_perm, serialized_perms):
-    perms_buffer = serialized_perms.encode('utf-8')
-    result = lib.is_permitted_from_json(required_perm, perms_buffer, len(perms_buffer))
-    if result == -1:
-        raise ValueError("Rust Library, libauthz, panicked!")
-    return bool(result)
-
-
-def is_permitted_from_str(required_perm, assigned_perms):
-    result = lib.is_permitted_from_str(required_perm, assigned_perm, len(assigned_perms))
-
-    if result == -1:
-        raise ValueError("Rust Library, libauthz, panicked!")
-    return bool(result)
+        if result == -1:
+            raise ValueError("Rust Library, libauthz, panicked!")
+        return bool(result)
